ffb3b987f67d40bf0efdca0d19314c33de04842f
yatish0492 <yatish0492@gmail.com>
2020-07-12 03:06:34 +0530

    Update J9_Join.java
    Updated the comments of 'Join'. Before comments was wrong explanation

--- java7Tutorial/src/com/yatish/Threads/J9_Join.java
+++ java7Tutorial/src/com/yatish/Threads/J9_Join.java
@@ -20,26 +20,15 @@ public class J9_Join {
 		o1.start();
 		
 		/*
-		 * we can achieve what synchronized does using join also.
+		 * 'o1.join()' --> This block the execution point here itself(at this statement) until 't1' thread is completed. like how while debugging
+		 * 		   until we release the control to next step. So if already we have started any other threads say like 'o2.start()' then 
+		 *		   that thread would still run in parallel. Basically it doesn't block any other threads which are running.
 		 * 
-		 * If we can achieve what synchronized does using 'join()' then why the hell do we need 'join()'?
-		 * Synchronized can be applied only at method level or at block level. consider you want the threads to be executed sequentially from start to end like once the thread is create by servlet 
-		 * handler until that thread returns back the response/dies, then only other thread should start executing. In this case the thread might be executing 100+ methods before dying/returning 
-		 * response, how good is it to put synchronized on all those 100+ methods. consider, you are so rude and dumb that you want to put synchronized on all the 100+ methods, then if you want to
-		 * execute only one particular thread like this sequentially and for other threads you don't want to execute them sequentially, how will synchronized on all 100+ methods achieve that.
-		 * this is where 'join()' comes into picture. 'join()' applies synchronized at the thread level. if we call 'join()' on the thread, then that thread will excuted completely by blocking any
-		 * other threads trying to start/execute.
-		 * 
-		 * NOTE: you have to call 'join' on thread like as shown below 'o1.join();', if have to call join on a thread once you start the thread 'o1' and before any other thread starts like before 
-		 * 		'o2.start()'. if it is called before 'o1.start()' or after 'o2.start()'. then it is useless, it doesn't do any synchronization.
-		 * 
-		 * In this case, since we have called '.join()' on 'o1', first 'o1' will be completely executed while 'o2' and 'o3' will be blocked. once 'o1' completes its execution, 'o2' and 'o3' will 
-		 * start executing without any synchronization since we have not called '.join()' on 'o2'.
-		 * 
-		 * 
-		 * NOTE: There is one more variation of '.join()' which accepts a parameter(long). '.join(long milliseconds)' in this case instead of waiting until the thread to finish completely,
-		 * 			JVM will execute the thread on which join is called for specified milliseconds by blocking other threads. once the specified milliseconds are 
-		 * 			done, then JVM will unblock all the threads hence again all will be executed parallely.
+		 * NOTE: There is one more variation of '.join()' which accepts a parameter(long). '.join(long milliseconds)' in this case the execution point
+		 *		is blocked until whichever of the following happens first,
+		 *			1) If the thread on which '.join()' is called finishes 
+		 *						OR
+		 *			2) if the mentioned 'milliseconds' times out.
 		 * 
 		 * 
 		 */
