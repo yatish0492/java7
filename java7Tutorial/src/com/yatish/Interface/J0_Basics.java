2c0b983d989b03c7a5688a2d6345ed096d949f7d
Yatish C S <ycs@hotels.com>
2020-07-23 00:53:52 +0530

    Addded Basic info about Interface

---
+++ java7Tutorial/src/com/yatish/Interface/J0_Basics.java
@@ -0,0 +1,8 @@
+package com.yatish.Interface;
+
+/*
+    * By default all the data members of interface are 'public static final'. Observe only data members not the methods.
+    * By default the methods are 'public abstract'.
+ */
+public class J0_Basics {
+}
